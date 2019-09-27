e68f5dfd7e654632b2533e98ffa69c73b5a7da1c
kiranPatel <kiranPatel@192.168.0.24>
2019-09-27 11:21:35 -0400

    next coommit

--- tedtGit/src/test/java/FbLogTest.java
+++ tedtGit/src/test/java/FbLogTest.java
@@ -1,32 +1,43 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 import Elements.AllElements;
 
 public class FbLogTest {
 	private static WebDriver driver = null;
-
+	ExtentHtmlReporter htmlReporter;
+	ExtentReports report;
+	ExtentTest test;
+	
+	
 	@BeforeTest
 	public void setup() {
+		 htmlReporter = new ExtentHtmlReporter("extentReport.html");
+		 report = new ExtentReports();
+		report.attachReporter(htmlReporter);
+		 test = report.createTest("Testing","This is for creating a test");
 		String project = System.getProperty("user.dir");
 		System.setProperty("webdriver.gecko.driver", project + "/Drivers/Firefox/geckodriver.exe");
 		driver = new FirefoxDriver();
+		test.log(Status.INFO, "Starting a test case");
 		driver.get("https://www.facebook.com/");
+		test.pass("Navigated to the facebook page");
 	}
 
 	@Test
-	public static void fbLoginTest() {
+	public void fbLoginTest() {
 
 		AllElements elements = new AllElements(driver);
 		elements.element_EmailTextbox("kp1041992@gmail.com");
-
+		test.pass("Email component found and insert value successfully");
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
@@ -34,6 +45,7 @@ public class FbLogTest {
 			e.printStackTrace();
 		}
 		elements.element_PasswordTextbox("abcd");
+		test.pass("password component found and insert value successfully");
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
@@ -41,14 +53,16 @@ public class FbLogTest {
 			e.printStackTrace();
 		}
 		elements.element_LoginBtn();
+		test.pass("Login button clicked");
 
 	}
 
 	@AfterTest
 	public void tearDown() {
 		driver.close();
-
-		System.out.println("test completed");
+		test.pass("Closed the browser");
+		test.info("test completed");
+		report.flush();
 	}
 
 	
